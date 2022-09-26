17bc13486e3d33de40493a873dbf3d0c1b903b0e
AngelDX <angel.condori@gmail.com>
2022-09-26 15:22:17 -0500

    Proyectos moviles

---
+++ app_restvcm/routes/api.php
@@ -0,0 +1,28 @@
+<?php
+
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\PersonaController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::group(['middleware' => 'api'], function () {
+    Route::group(['prefix' => 'auth'], function () {
+        Route::post('/login', [AuthController::class, 'login']);
+        Route::post('/register', [AuthController::class, 'register']);
+    });
+    Route::post('/logout', [AuthController::class, 'logout']);
+    Route::post('/refresh', [AuthController::class, 'refresh']);
+    Route::get('/profile', [AuthController::class, 'userProfile']);  
+    Route::resource('/persona', PersonaController::class);  
+});
