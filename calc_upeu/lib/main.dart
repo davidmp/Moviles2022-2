0ac7547c9350d8a2cd9a61119cf2e2e90076b031
AngelDX <angel.condori@gmail.com>
2022-10-31 15:26:43 -0500

    Calculadora upeu y colores

--- calc_upeu/lib/main.dart
+++ calc_upeu/lib/main.dart
@@ -1,13 +1,35 @@
 // @dart=2.9
 import 'package:flutter/material.dart';
 import './comp/CalcButton.dart';
+import 'dart:math';
 void main() => runApp(CalcApp());
 class CalcApp extends StatefulWidget {
   const CalcApp({Key key}) : super(key: key);
   @override
   CalcAppState createState() => CalcAppState();
 }
-
+const Color m3BaseColorD = Color(0xff99ff05);
+const Color m3BaseColor = Color(0xff6750a4);
+const List<Color> colorOptions = [
+  m3BaseColor,
+  m3BaseColorD,
+  Colors.blue,
+  Colors.teal,
+  Colors.green,
+  Colors.yellow,
+  Colors.orange,
+  Colors.pink
+];
+const List<String> colorText = <String>[
+  "M3 Baseline",
+  "M3 BaselineD",
+  "Blue",
+  "Teal",
+  "Green",
+  "Yellow",
+  "Orange",
+  "Pink",
+];
 class CalcAppState extends State<CalcApp> {
   bool useMaterial3 = true;
   bool useLightMode = true;
@@ -34,6 +56,28 @@ class CalcAppState extends State<CalcApp> {
       _controller.text = '';
     });
   }
+
+  void valClickPi(String text) {
+    setState(() => _controller.text ="${pi}");
+    print(_controller);
+  }
+
+  void raizCuadrada(String text) {
+    setState((){
+      if(_controller.text!=null) {
+        _controller.text = "${sqrt(int.parse(_controller.text))}";
+      }
+    });
+  }
+
+  void potenciaDos(String text) {
+    setState((){
+      if(_controller.text!=null) {
+        _controller.text = "${pow(int.parse(_controller.text),2)}";
+      }
+    });
+  }
+
 //Aqui codigo
   void resultOperacion(String text) {
     setState(() {
@@ -58,14 +102,18 @@ class CalcAppState extends State<CalcApp> {
           _controller.text =
               (int.parse(valorAnt) % int.parse(_controller.text)).toString();
           break;
+        case "x²":
+          _controller.text =
+              (pow(int.parse(valorAnt) , int.parse(_controller.text))).toString();
+          break;
       }
     });
   }
   @override
   Widget build(BuildContext context) {
 
-    List<List> labelList = [["AC","C","%","/"],["7","8","9","*"],["4","5","6","-"],["1","2","3","+"],[".","0","00","="]];
-    List<List> funx=[[clear,clear, opeClick,opeClick ],
+    List<List> labelList = [["AC","C","%","/"],["π","√","n²","x²"],["7","8","9","*"],["4","5","6","-"],["1","2","3","+"],[".","0","00","="]];
+    List<List> funx=[[clear,clear, opeClick,opeClick ], [valClickPi,raizCuadrada, potenciaDos,opeClick ],
       [numClick,numClick, numClick,opeClick ],
       [numClick,numClick, numClick,opeClick ],
       [numClick,numClick, numClick,opeClick ],
@@ -74,12 +122,15 @@ class CalcAppState extends State<CalcApp> {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Calculator',
-      theme:ThemeData(
-        primarySwatch: Colors.deepOrange,
+      themeMode: useLightMode ? ThemeMode.light : ThemeMode.dark,
+      theme: themeData,
+      /*theme:ThemeData(
+        primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
+      ),*/
       home: Scaffold(
-        body: Container(
+        appBar: createAppBar(),
+        body: SingleChildScrollView(
           padding: EdgeInsets.all(20),
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
@@ -114,6 +165,80 @@ class CalcAppState extends State<CalcApp> {
           ),
         ),
       ),
+    );
+  }
+
+  @override
+  initState() {
+    super.initState();
+    themeData = updateThemes(colorSelected, useMaterial3, useLightMode);
+  }
+  ThemeData updateThemes(int colorIndex, bool useMaterial3, bool useLightMode) {
+    return ThemeData(
+        colorSchemeSeed: colorOptions[colorSelected],
+        useMaterial3: useMaterial3,
+        brightness: useLightMode ? Brightness.light : Brightness.dark);
+  }
+  void handleBrightnessChange() {
+    setState(() {
+      useLightMode = !useLightMode;
+      themeData = updateThemes(colorSelected, useMaterial3, useLightMode);
+    });
+  }
+  void handleMaterialVersionChange() {
+    setState(() {
+      useMaterial3 = !useMaterial3;
+      themeData = updateThemes(colorSelected, useMaterial3, useLightMode);
+    });
+  }
+  void handleColorSelect(int value) {
+    setState(() {
+      colorSelected = value;
+      themeData = updateThemes(colorSelected, useMaterial3, useLightMode);
+    });
+  }
+
+  PreferredSizeWidget createAppBar() {
+    return AppBar(
+      title: useMaterial3 ? const Text("Material 3") : const Text("Material 2"),
+      actions: [
+        IconButton(
+          icon: useLightMode? const Icon(Icons.wb_sunny_outlined): const Icon(Icons.wb_sunny),
+          onPressed: handleBrightnessChange,
+          tooltip: "Toggle brightness",
+        ),
+        IconButton(
+          icon: useMaterial3? const Icon(Icons.filter_3): const Icon(Icons.filter_2),
+          onPressed: handleMaterialVersionChange,
+          tooltip: "Switch to Material ${useMaterial3 ? 2 : 3}",
+        ),
+        PopupMenuButton(
+          icon: const Icon(Icons.more_vert),
+          shape:
+          RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+          itemBuilder: (context) {
+            return List.generate(colorOptions.length, (index) {
+              return PopupMenuItem(
+                  value: index,
+                  child: Wrap(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.only(left: 10),
+                        child: Icon(
+                          index == colorSelected? Icons.color_lens: Icons.color_lens_outlined,
+                          color: colorOptions[index],
+                        ),
+                      ),
+                      Padding(
+                          padding: const EdgeInsets.only(left: 20),
+                          child: Text(colorText[index]))
+                    ],
+                  ));
+            });
+          },
+          onSelected: handleColorSelect,
+        ),
+      ],
     );
   }
 
