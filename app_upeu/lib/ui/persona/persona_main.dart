90a0751cc41333167dd35607e2ced7fa9b2d6ad6
AngelDX <angel.condori@gmail.com>
2022-11-09 15:19:18 -0500

    CRUD persona

--- app_upeu/lib/ui/persona/persona_main.dart
+++ app_upeu/lib/ui/persona/persona_main.dart
@@ -1,5 +1,7 @@
 import 'package:app_upeu/apis/persona_api.dart';
 import 'package:app_upeu/comp/TabItem.dart';
+import 'package:app_upeu/ui/persona/persona_edit.dart';
+import 'package:app_upeu/ui/persona/persona_form.dart';
 //import 'package:app_upeu/ui/persona/beneficiario_edit.dart';
 //import 'package:app_upeu/ui/beneficiario/beneficiario_form.dart';
 import 'package:flutter/material.dart';
@@ -89,10 +91,10 @@ class _PersonaUIState extends State<PersonaUI> {
                   //final producto=new ModeloProductos();
                   //formDialog(context, producto);
                   print("Si funciona 2");
-                  /*Navigator.push(
+                  Navigator.push(
                     context,
                     MaterialPageRoute(builder: (context) => PersonaForm()),
-                  ).then(onGoBack);*/
+                  ).then(onGoBack);
                 },
                 child: Icon(Icons.add_box_sharp),
               ),
@@ -152,7 +154,7 @@ class _PersonaUIState extends State<PersonaUI> {
                               Container(
                                 decoration: BoxDecoration(
                                     borderRadius: BorderRadius.circular(8),
-                                    color: Colors.redAccent),
+                                    color: AppTheme.themeData.colorScheme.primaryContainer),
                                 child: Text(
                                   personax.dni,
                                   style: TextStyle(
@@ -162,7 +164,7 @@ class _PersonaUIState extends State<PersonaUI> {
                               Container(
                                 decoration: BoxDecoration(
                                     borderRadius: BorderRadius.circular(8),
-                                    color: Colors.yellow),
+                                    color: AppTheme.themeData.colorScheme.primaryContainer),
                                 child: Text(
                                   personax.genero.toString(),
                                   style: TextStyle(
@@ -179,16 +181,18 @@ class _PersonaUIState extends State<PersonaUI> {
                             children: <Widget>[
                               IconButton(
                                   icon: Icon(Icons.edit),
+                                  color: AppTheme.themeData.colorScheme.primary,
                                   onPressed: () {
-                                    /*Navigator.push(
+                                    Navigator.push(
                                       context,
                                       MaterialPageRoute(
                                           builder: (context) => PersonaFormEdit(
                                               modelP: personax)),
-                                    ).then(onGoBack);*/
+                                    ).then(onGoBack);
                                   }),
                               IconButton(
                                   icon: Icon(Icons.delete),
+                                  color: AppTheme.themeData.colorScheme.primary,
                                   onPressed: () {
                                     showDialog(
                                         context: context,
@@ -217,11 +221,11 @@ class _PersonaUIState extends State<PersonaUI> {
                                         }).then((value) {
                                       if (value.toString() == "Success") {
                                         print(personax.id);
-                                        /*
+
                                         Provider.of<PersonaApi>(context,
                                             listen: false)
-                                            .deleteBeneficiario(personax.id)
-                                            .then((value) => onGoBack(value));*/
+                                            .deletePersona(TokenUtil.TOKEN,personax.id)
+                                            .then((value) => onGoBack(value));
                                         //var onGoBack = onGoBack;
                                         //BlocProvider.of<ProductosBloc>(context).add(DeleteProductoEvent(producto: state.productosList[index]));
                                       }
@@ -243,7 +247,7 @@ class _PersonaUIState extends State<PersonaUI> {
   int selectedPosition = 0;
   _buildBottomTab() {
     return BottomAppBar(
-      color: Colors.deepPurple,
+      color: AppTheme.themeData.colorScheme.primary,
       shape: CircularNotchedRectangle(),
       child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
